e5c8c324375a71ee03ecf43dc5e54e97a69f5e1d
OcztosKaroly <126339777+OcztosKaroly@users.noreply.github.com>
2024-10-27 19:56:45 +0100

    Notification fix
    Daily notification now started when no other daily notification started.

--- app/src/main/java/com/example/magyar_madarak/HunBirdApplication.java
+++ app/src/main/java/com/example/magyar_madarak/HunBirdApplication.java
@@ -1,12 +1,16 @@
 package com.example.magyar_madarak;
 
 import android.app.Application;
+import android.util.Log;
 
 import androidx.work.PeriodicWorkRequest;
+import androidx.work.WorkInfo;
 import androidx.work.WorkManager;
 
 import com.example.magyar_madarak.workers.NotificationWorker;
+import com.google.common.util.concurrent.ListenableFuture;
 
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 public class HunBirdApplication extends Application {
@@ -17,10 +21,27 @@ public class HunBirdApplication extends Application {
 
 //        WorkManager.getInstance(this).cancelAllWork();
 
-        PeriodicWorkRequest dailyWorkRequest = new PeriodicWorkRequest.Builder(
-                NotificationWorker.class,
-                24,
-                TimeUnit.HOURS).addTag("daily_notification").build();
-        WorkManager.getInstance(this).enqueue(dailyWorkRequest);
+        if (!isRunningNotificationByTag("daily_notification")) {
+            PeriodicWorkRequest dailyWorkRequest = new PeriodicWorkRequest.Builder(
+                    NotificationWorker.class,
+                    24,
+                    TimeUnit.HOURS).addTag("daily_notification").build();
+            WorkManager.getInstance(this).enqueue(dailyWorkRequest);
+        }
+    }
+
+    private boolean isRunningNotificationByTag(String tag) {
+        ListenableFuture<List<WorkInfo>> future = WorkManager.getInstance(this).getWorkInfosByTag(tag);
+        try {
+            List<WorkInfo> workInfos = future.get();
+            for (WorkInfo workInfo : workInfos) {
+                if (workInfo.getState() == WorkInfo.State.RUNNING || workInfo.getState() == WorkInfo.State.ENQUEUED) {
+                    return true;
+                }
+            }
+        } catch (Exception e) {
+            Log.e("NOTIFICATION", "--Error occurred when tried to get running notifications.--", e);
+        }
+        return false;
     }
 }
